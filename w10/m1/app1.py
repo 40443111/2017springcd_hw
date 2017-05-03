af7a12dc5d28d16a08e8dff31eaaf21e6f2eb5a1
40443111 <40443111@gm.nfu.edu.tw>
2017-05-04 03:16:13 +0800

    al

---
+++ w10/m1/app1.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append("./m1")
+import add
+
+
+sum = add.add(1, 2)
+
+print(sum)
---
+++ w10/m1/app2.py
@@ -0,0 +1,7 @@
+import sys 
+sys.path.append("./m1") 
+import mul 
+
+multiply = mul.mul(3, 5) 
+
+print(multiply)
